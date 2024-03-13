aeddd355a2bdf5acc9fb70fb1a4ca5f808f060cf
dhav211 <dhav211@gmail.com>
2024-03-12 20:46:02 -0700

    Install annotate gem

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: kittens
+#
+#  id         :integer          not null, primary key
+#  age        :integer
+#  cuteness   :integer
+#  image_url  :string
+#  name       :string
+#  softness   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Kitten < ApplicationRecord
 end
--- test/models/kitten_test.rb
+++ test/models/kitten_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: kittens
+#
+#  id         :integer          not null, primary key
+#  age        :integer
+#  cuteness   :integer
+#  image_url  :string
+#  name       :string
+#  softness   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class KittenTest < ActiveSupport::TestCase
